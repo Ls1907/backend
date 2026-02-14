88e173dd3aa514ce4a20ddc27651ff9bf132d50f
Kunwar Lav singh <dpsvaranasi619@gmail.com>
2026-02-14 19:09:43 +0530

    latest

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ import connectDB from "./db/indexdb.js"
 
 connectDB()
 .then(()=>{
+    app.on("error",(error)=>{
+        console.log("error")
+        throw error
+    })
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`Server listening on port ${process.env.PORT}`)
     });
