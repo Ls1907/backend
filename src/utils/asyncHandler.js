72544b94a1d24e488838414e8d60adc69b9cf273
Kunwar Lav singh <dpsvaranasi619@gmail.com>
2026-02-19 01:27:42 +0530

    till router and controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -10,10 +10,11 @@
 //     }
 // }
 
-const asyncHandler = (requestHandler)=> async (req,res,next)=>{
+const asyncHandler = (requestHandler)=>   {return (req,res,next)=>{
     Promise
     .resolve(requestHandler(req,res,next))
     .catch((err)=>next(err))
+}
 }
 
 export {asyncHandler}
